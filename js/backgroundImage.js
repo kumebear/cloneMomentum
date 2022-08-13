c5479476348aaf9ba7fcfaa6aa73b150bcae8aff
kumebear <kumebear@gmail.com>
2022-08-13 10:15:56 +0900

    log in-out

---
+++ js/backgroundImage.js
@@ -0,0 +1,11 @@
+images=[0,1,2,3];
+
+
+const select = Math.floor(Math.random()*images.length);
+
+
+
+
+document.createElement("img");
+
+
---
+++ js/login.js
@@ -0,0 +1,27 @@
+const loginForm=document.querySelector("#login-form");
+const loginInput = loginForm.querySelector("h2 input");
+const logined = document.querySelector("#logined");
+function handlerLogin(event){
+    const user = loginInput.value;
+    localStorage.setItem("user",user);
+    
+
+}
+function handlerLogout (){
+   localStorage.removeItem("user");
+    location.reload();
+}
+
+const user = localStorage.getItem("user");
+if(user!== null){
+    const btn=document.createElement("button");
+    btn.innerText="❌";
+    logined.innerText=`hello ${user}`;
+    logined.appendChild(btn);
+    loginForm.classList.add("hidden")
+    logined.classList.remove("hidden");
+    btn.addEventListener("click",handlerLogout);
+
+}
+
+loginForm.addEventListener("submit",handlerLogin);
